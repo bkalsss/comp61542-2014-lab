68279d4b247428258f6e83493a6b284ae3eda490
George Oxinos <mbaxkgo2@E-C07KI1803.cs.man.ac.uk>
2014-03-23 16:47:54 +0000

    first author test

---
+++ test/test_first_author.py
@@ -0,0 +1,20 @@
+'''
+Created on 23 Mar 2014
+
+@author: mbaxkgo2
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
